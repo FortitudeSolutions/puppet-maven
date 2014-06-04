e9463e52eb01af750f08fc4def81c172ca2d3a4f
Michael Nishizawa <michael.nishizawa@spectrumhealth.org>
2014-06-04 09:34:35 -0400

    removed logging

--- files/create_settings_security.sh
+++ files/create_settings_security.sh
@@ -2,7 +2,4 @@
 password=`date +%s | shasum -a 256 | base64 | head -c 32` 
 master_password=`mvn --encrypt-master-password $password | grep -o '{.*}'`
 
-echo "password=$password" > /tmp/mvnsecurity.log
-echo "encrypted=$master_password" >> /tmp/mvnsecurity.log
-
 echo "<settingsSecurity>\n<master>${master_password}</master>\n</settingsSecurity>" > ~/.m2/settings-security.xml
